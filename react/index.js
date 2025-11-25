1d323779b22685d3300e5bfe07ad7ce505d2c923
canlooks <364021661@qq.com>
2025-11-25 20:19:38 +0800

    1

--- react/index.js
+++ react/index.js
@@ -1,1 +1,4 @@
-export * from '../dist/esm/react';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+tslib_1.__exportStar(require("../dist/cjs/react"), exports);
