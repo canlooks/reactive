b35da7d93f6fe848c5d080e958cc9d73b617a207
canlooks <364021661@qq.com>
2025-12-08 12:17:44 +0800

    Revert "1"
    This reverts commit a12cce923cd6cb0204c296e9ceca6ef6a9308859.

--- test/main.mobx.tsx
+++ test/main.mobx.tsx
@@ -1,16 +1,18 @@
 import React, {useDeferredValue} from 'react'
 import {createRoot} from 'react-dom/client'
 import {Autoload, loading, reactive} from '../src'
-import {RC, useReactive, useReactor} from '../src/react'
+import {makeAutoObservable} from 'mobx'
+import {observer} from './mobx-react-lite'
 
-@reactive
-class TestStore extends Autoload {
-    loadData() {
-        return new Promise(resolve => setTimeout(() => resolve(123), 1000))
+class TestStore {
+    constructor() {
+        makeAutoObservable(this)
     }
 
-    get double() {
-        return this.data && this.data * 2
+    async loadData() {
+        console.log('loadData')
+        await new Promise(resolve => setTimeout(resolve, 1000))
+        return 'World'
     }
 
     showChild = false
@@ -19,13 +21,7 @@ class TestStore extends Autoload {
 
 const testStore = new TestStore()
 
-const App = RC(function App() {
-    const state = useReactive({
-        get many() {
-            return testStore.double && testStore.double * 3
-        }
-    })
-
+const App = observer(function App() {
     console.log('app')
     const onClick = () => {
         testStore.showChild = true
@@ -36,17 +32,13 @@ const App = RC(function App() {
 
     const deferredShowChild = useDeferredValue(testStore.showChild)
 
-    useReactor(() => state.many, () => {
-        console.log(state.many)
-    })
-
     return (
         <>
-            <h1>Hello {testStore.data} {state.many}</h1>
+            {/*<h1>Hello {testStore.data}</h1>*/}
             <button onClick={onClick}>button</button>
-            {testStore.loading &&
-                <h2>Loading...</h2>
-            }
+            {/*{testStore.loading &&*/}
+            {/*    <h2>Loading...</h2>*/}
+            {/*}*/}
             {deferredShowChild &&
                 <Child/>
             }
@@ -54,7 +46,7 @@ const App = RC(function App() {
     )
 })
 
-const Child = RC(function Child() {
+const Child = observer(function Child() {
     console.log('Child')
     return Array(10_000).fill(void 0).map((_, i) =>
         <div key={i}>{i * testStore.multiple}</div>
